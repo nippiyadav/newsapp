018379858dfd1d3f75db2d515adf677cb2a6704b
chandan yadav <nippiyadav890@gmail.com>
2024-10-23 08:44:41 +0530

    change in type in button and some register etc

--- app/(components)/Button.jsx
+++ app/(components)/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({type="submit",text,className}) {
   return (
-    <button className={`${className}`}>{text}</button>
+    <button type={type} className={`${className}`}>{text}</button>
   )
 }
 
